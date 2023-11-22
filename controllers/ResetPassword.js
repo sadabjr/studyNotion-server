152ebfd5b53cc5b63df59713b5c66306cdf0655f
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2023-11-22 10:14:21 +0530

    reset password

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -1,5 +1,103 @@
+const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
+const bcrypt = require("bcrypt");
+
 // resetPasswordToken(reset password mail sender)
+exports.resetPasswordToken = async (req, res) => {
+  try {
+    // get email from req body
+    const email = req.body.email;
 
+    // check user fro this email, email validation
+    const user = await User.findOne({ email: email });
+    if (!User) {
+      return res.json({
+        success: false,
+        message: "Your Email is not registered with us",
+      });
+    }
 
+    // generate token
+    const token = crypto.randomUUID();
+    // update user by adding token and expiring time
+    const updateDetails = await User.findOneAndUpdate(
+      { email: email },
+      {
+        token: token,
+        resetPasswordExpires: Date.now() + 5 * 60 * 100,
+      },
+      { new: true }
+    );
+    // create url
+    const url = `http://localhost:3000/update-password/${token}`;
+
+    // send mail containing the url
+    await mailSender(
+      email,
+      "Password Reset Link",
+      `Password Reset Link: ${url}`
+    );
+    // return response
+    return res.json({
+      success: true,
+      message:
+        "Email send successfully, PLease check email and change Password",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while sending reset password mail",
+    });
+  }
+};
 
 // reset password(in database)
+exports.resetPassword = async (req, res) => {
+  try {
+    // data fetch
+    const { password, confirmPassword, token } = req.body;
+    // validation
+    if (password !== confirmPassword) {
+      return res.json({
+        success: false,
+        message: "Password is not matched",
+      });
+    }
+    // get user details from db using token
+    const userDetails = await User.findOne({ token: token });
+    // if no entry invalid token
+    if (!userDetails) {
+      return res.json({
+        success: false,
+        message: "Invalid Token",
+      });
+    }
+    // token time check
+    if (userDetails.resetPasswordExpires < Date.now()) {
+      return res.json({
+        success: false,
+        message: "Token is expired, Please regenerate your token!",
+      });
+    }
+
+    // hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // password update
+    await User.findOneAndUpdate(
+      { token: token },
+      { password: hashPassword },
+      { new: true }
+    );
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Password reset successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+  }
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,24 +6,91 @@ require("dotenv").config;
 exports.auth = async (req, res, next) => {
   try {
     // extract token
-    const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer", "")
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorisation").replace("Bearer", "");
 
     // if token is missing , send response
-    if(!token){
-        return req.status("403").json({
-            success:false,
-            message:"Token is missing"
-        })
+    if (!token) {
+      return req.status(401).json({
+        success: false,
+        message: "Token is missing",
+      });
     }
 
-  }
-  catch (error) {
+    // verify the token
+    try {
+      const decode = jwt.verify(token, process.JWT_SECRET);
+      console.log(decode);
+      req.user = decode;
+    } catch (err) {
+      // verification issue
+      return res.status(401).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+    next();
+  } catch (error) {
     console.log(error);
+    return res.status(401).json({
+      success: false,
+      message: "Something went wrong while validating the token",
+    });
   }
 };
 
-// isStudent
+// isStudent\
+exports.isStudent = (req, res, next) => {
+try{
+if(req.user.accountType !== "Student"){
+  return res.status(401).json({
+    success:false,
+    message:"This is a protected route for student only"
+  })
+}
+}
+catch(error){
+  return res.status(401).json({
+    success:false,
+    message:"user role can not be verified, Please try again"
+  })
+}
+}
 
 // isInstructor
+exports.isInstructor = (req, res, next) => {
+  try{
+  if(req.user.accountType !== "Instructor"){
+    return res.status(401).json({
+      success:false,
+      message:"This is a protected route for Instructor only"
+    })
+  }
+  }
+  catch(error){
+    return res.status(401).json({
+      success:false,
+      message:"user role can not be verified, Please try again"
+    })
+  }
+  }
 
 // isAdmin
+exports.isAdmin = (req, res, next) => {
+  try{
+  if(req.user.accountType !== "Admin"){
+    return res.status(401).json({
+      success:false,
+      message:"This is a protected route for Admin only"
+    })
+  }
+  }
+  catch(error){
+    return res.status(401).json({
+      success:false,
+      message:"user role can not be verified, Please try again"
+    })
+  }
+  }
